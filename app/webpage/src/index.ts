c72c9fc0124796e3ffc4410f071ceed69c4e073e
alex <petrovich.alex96@gmail.com>
2023-02-27 13:22:19 -0600

    piggo-legends webpage (#4)

---
+++ app/webpage/src/index.ts
@@ -0,0 +1,20 @@
+import { Application, Graphics } from 'pixi.js'
+import '@pixi/unsafe-eval';
+
+const app = new Application({
+    view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+    resolution: window.devicePixelRatio || 1,
+    autoDensity: true,
+    backgroundColor: 0x6495ed,
+    width: 800,
+    height: 600
+});
+
+const graphy: Graphics = new Graphics();
+
+graphy.beginFill(0xFF00FF);
+graphy.lineStyle(10, 0x00FF00);
+graphy.drawCircle(200, 200, 25);
+graphy.endFill();
+
+app.stage.addChild(graphy);
---
+++ app/webpage/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: ['./src/index.ts'],
+  mode: 'development',
+  stats: 'minimal',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  optimization: {
+    minimize: true,
+
+  },
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.join(__dirname, "src", "*.html"), to: path.join(__dirname, "dist", "[name].html") },
+        {
+          from: "./src/*.html",
+          to: () => "[name].html"
+        },
+        {
+          from: "./src/*.css",
+          to: () => "[name].css"
+        },
+        {
+          from: "./src/favicon.ico",
+          to: () => "favicon.ico"
+        }
+      ],
+    })
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'piggo-legends-min.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+};
