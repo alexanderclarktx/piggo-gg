eed0ab3beb29b8e5d0ebaacabb1e5fddfeb19540
alexanderclarktx <petrovich.alex96@gmail.com>
2023-03-01 17:56:30 -0600

    browser webrtc handshake

--- app/webpage/src/index.ts
+++ app/webpage/src/index.ts
@@ -21,3 +21,43 @@ graphy.endFill();
 app.stage.addChild(graphy);
 
 // import the webrtc library
+const client = new WebRTC();
+window["client"] = client;
+
+// accept an offer
+const buttonAcceptOffer = document.getElementById("button-accept-offer") as HTMLButtonElement
+const inputAcceptOffer = document.getElementById("input-accept-offer") as HTMLInputElement
+const pAnswer = document.getElementById("p-answer") as HTMLParagraphElement
+
+buttonAcceptOffer.onclick = async () => {
+    const offer = atob(inputAcceptOffer.value);
+    const answer = await client.acceptOffer(offer);
+    pAnswer.innerText = btoa(answer);
+}
+
+// send an offer
+const buttonSendOffer = document.getElementById("button-send-offer") as HTMLButtonElement
+const pOffer = document.getElementById("p-offer") as HTMLParagraphElement
+
+// accept an answer
+const inputAnswer = document.getElementById("input-answer") as HTMLInputElement
+const buttonAnswer = document.getElementById("button-answer") as HTMLButtonElement
+
+buttonSendOffer.onclick = async () => {
+    client.createOffer((offer: string) => {
+        pOffer.innerText = btoa(offer);
+
+        // unhide the answer input
+        inputAnswer.hidden = false;
+        buttonAnswer.hidden = false;
+
+        // hide accept offer
+        buttonAcceptOffer.hidden = true;
+        inputAcceptOffer.hidden = true;
+    });
+}
+
+buttonAnswer.onclick = async () => {
+    const answer = atob(inputAnswer.value);
+    await client.acceptAnswer(answer);
+}
