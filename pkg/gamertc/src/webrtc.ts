20be2e2a9494c47c8f0d7cf27bc7ed96361205d0
alexanderclarktx <petrovich.alex96@gmail.com>
2023-03-02 15:55:14 -0600

    try public turn server

--- pkg/gamertc/src/webrtc.ts
+++ pkg/gamertc/src/webrtc.ts
@@ -7,7 +7,12 @@ export class WebRTC {
       { urls: "stun:stun1.l.google.com:19302" },
       { urls: "stun:stun2.l.google.com:19302" },
       { urls: "stun:stun3.l.google.com:19302" },
-      { urls: "stun:stun4.l.google.com:19302" }
+      { urls: "stun:stun4.l.google.com:19302" },
+      {
+        url: 'turn:turn.bistri.com:80',
+        credential: 'homeo',
+        username: 'homeo'
+      }
     ]
   }
   dataChannelSettings = {
