2e75bdf1c28cdec7b6ebf091b5350aa1445ae8b0
alexanderclarktx <petrovich.alex96@gmail.com>
2023-03-02 15:46:30 -0600

    try with more iceServer

--- pkg/gamertc/src/webrtc.ts
+++ pkg/gamertc/src/webrtc.ts
@@ -2,7 +2,13 @@
 
 export class WebRTC {
   config = {
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+    iceServers: [
+      { urls: "stun:stun.l.google.com:19302" },
+      { urls: "stun:stun1.l.google.com:19302" },
+      { urls: "stun:stun2.l.google.com:19302" },
+      { urls: "stun:stun3.l.google.com:19302" },
+      { urls: "stun:stun4.l.google.com:19302" }
+    ]
   }
   dataChannelSettings = {
     reliable: {
