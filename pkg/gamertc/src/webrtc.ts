6c4bb10dcb7fc36ff975cd20cf9bd148a0f310a7
alexanderclarktx <petrovich.alex96@gmail.com>
2023-03-02 16:20:03 -0600

    pages

--- pkg/gamertc/src/webrtc.ts
+++ pkg/gamertc/src/webrtc.ts
@@ -4,11 +4,10 @@ export class WebRTC {
   config = {
     iceServers: [
       { urls: "stun:stun.l.google.com:19302" },
-      { urls: "stun:stun3.l.google.com:19302" },
       {
-        urls: 'turn:turn.bistri.com:80',
-        credential: 'homeo',
-        username: 'homeo'
+        urls: "turn:turn.anyfirewall.com:443?transport=tcp",
+        credential: "webrtc",
+        username: "webrtc"
       }
     ]
   }
