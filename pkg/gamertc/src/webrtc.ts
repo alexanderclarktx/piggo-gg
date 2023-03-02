7b67d10338cd687b9d233cda8fb47b863ae16fcf
alexanderclarktx <petrovich.alex96@gmail.com>
2023-03-02 16:02:02 -0600

    listen to browser warning

--- pkg/gamertc/src/webrtc.ts
+++ pkg/gamertc/src/webrtc.ts
@@ -4,10 +4,7 @@ export class WebRTC {
   config = {
     iceServers: [
       { urls: "stun:stun.l.google.com:19302" },
-      { urls: "stun:stun1.l.google.com:19302" },
-      { urls: "stun:stun2.l.google.com:19302" },
       { urls: "stun:stun3.l.google.com:19302" },
-      { urls: "stun:stun4.l.google.com:19302" },
       {
         urls: 'turn:turn.bistri.com:80',
         credential: 'homeo',
