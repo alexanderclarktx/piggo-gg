95bacecd651ae9da9481cee8e7239c530572819e
Alex <petrovich.alex96@gmail.com>
2024-03-07 19:36:52 +0800

    dev

--- games/src/Soccer.ts
+++ games/src/Soccer.ts
@@ -39,6 +39,20 @@ export const Soccer: WorldBuilder = (props) => {
   world.addEntity(Goal({ id: "goal1", color: 0xff0000, position: { x: 200, y: 500 }, width: 100, length: 2 }));
   world.addEntity(Goal({ id: "goald2", color: 0x0000ff, position: { x: 500, y: 200 }, width: 100, length: 2 }));
 
+  // an 8 sided shape with elongated side walls
+  const worldPointsFromScreen = [
+    [ 0, 0 ],
+    [ 0, 400 ],
+    [ 400, 400 ],
+    [ 400, 0 ],
+    [ 200, -200 ],
+    [ -200, 0 ],
+    [ -200, 200 ],
+    [ 0,  ],
+  ]
+
+  const worldPoints = worldPointsFromScreen.map(([x, y]) => ([ x + 400, y + 400 ]));
+
   // walls
   world.addEntities([
     // Wall({ x: 420, y: -20, length: 420, width: 0 }),
@@ -46,17 +60,19 @@ export const Soccer: WorldBuilder = (props) => {
     // Wall({ x: 420, y: 780, length: 420, width: 0 }),
     // Wall({ x: 815, y: 380, length: 0, width: 420 }),
 
-    LineWall({ points: [
-      250, 550,
-      410, 605,
-      630, 393,
-      550, 250,
-      445, 145,
-      280, 90,
-      90, 280,
-      145, 445,
-      250, 550
-    ]}),
+    LineWall({ points: worldPoints.flat() }),
+
+    // LineWall({ points: [
+    //   250, 550,
+    //   400, 580,
+    //   580, 400,
+    //   550, 250,
+    //   445, 145,
+    //   280, 90,
+    //   90, 280,
+    //   145, 445,
+    //   250, 550
+    // ]}),
   ]);
 
   return world;
