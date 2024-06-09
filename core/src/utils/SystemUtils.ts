9a82a1846b0157013d9f9fb72b58dafc38dcc49d
Alex <petrovich.alex96@gmail.com>
2024-06-09 12:37:57 -0400

    cozy

--- core/src/utils/SystemUtils.ts
+++ core/src/utils/SystemUtils.ts
@@ -1,5 +1,3 @@
 export const isMobile = (): boolean => /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-export const keys = Object.keys;
-export const values = Object.values;
-export const entries = Object.entries;
+export const { keys, values, entries } = Object;
