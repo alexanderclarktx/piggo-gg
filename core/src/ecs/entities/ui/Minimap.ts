83982162001c8c43c635815773517d1920c5ef93
Alex <petrovich.alex96@gmail.com>
2024-07-20 15:50:30 -0500

    other player dots

--- core/src/ecs/entities/ui/Minimap.ts
+++ core/src/ecs/entities/ui/Minimap.ts
@@ -1,18 +1,19 @@
-import { Action, Actions, Debug, Entity, Input, Position, Renderable, TeamColors, pixiGraphics } from "@piggo-gg/core";
-import { Container } from "pixi.js";
+import { Action, Actions, Debug, Entity, Input, Position, Renderable, TeamColors, keys, pixiGraphics, values } from "@piggo-gg/core";
+import { Container, Graphics } from "pixi.js";
 
 export const Minimap = (dim: number, tileMap: number[]): Entity => {
   let scale = 0.5;
   let fullscreen = false;
+
+  const dots: Record<string, Graphics> = {};
 
   const container = new Container();
   const tileGraphics = pixiGraphics({ alpha: 0.9, rotation: Math.PI / 4 });
   const background = pixiGraphics();
   const mask = background.clone();
   const outline = pixiGraphics();
-  const playerDot = pixiGraphics().circle(0, 0, 3).fill({ color: 0x00ff00 });
 
-  background.circle(0, 0, 100).fill({ color: 0x000000, alpha: 0.4 });  
+  background.circle(0, 0, 100).fill({ color: 0x000000, alpha: 0.4 });
   outline.circle(0, 0, 100).stroke({ color: 0xffffff, width: 2, alpha: 0.9 });
 
   const Colors: Record<number, number> = {
@@ -35,19 +36,26 @@ export const Minimap = (dim: number, tileMap: number[]): Entity => {
           if (fullscreen) {
             tileGraphics.mask = null;
             tileGraphics.scale = 1.5;
+            values(dots).forEach((dot) => dot.mask = null);
 
-            const bounds = container.getBounds();
+            const bounds = tileGraphics.getBounds();
 
             const x = (world.renderer?.app.canvas.width ?? 0) / 2;
-            const y = ((world.renderer?.app.canvas.height ?? 0) - bounds.height - 100) / 2;
-            minimap.components.position = Position({ x, y, screenFixed: true });
+            const y = Math.max(0, ((world.renderer?.app.canvas.height ?? 0) - bounds.height) / 2 - 100);
+
+            minimap.components.position.data.x = x;
+            minimap.components.position.data.y = y;
 
             background.clear();
             outline.clear();
           } else {
-            minimap.components.position = Position({ x: -125, y: 125, screenFixed: true });
             tileGraphics.mask = mask;
             tileGraphics.scale = 1;
+            values(dots).forEach((dot) => dot.mask = mask);
+
+            minimap.components.position.data.x = -125;
+            minimap.components.position.data.y = 125;
+
             background.circle(0, 0, 100).fill({ color: 0x000000, alpha: 0.4 });
             outline.circle(0, 0, 100).stroke({ color: 0xffffff, width: 2, alpha: 0.9 });
           }
@@ -56,22 +64,54 @@ export const Minimap = (dim: number, tileMap: number[]): Entity => {
       renderable: Renderable({
         zIndex: 10,
         dynamic: (_, __, ___, w) => {
-          const entityId = w.client?.playerEntity.components.controlling.data.entityId;
-          if (!entityId) return;
 
-          const entity = w.entities[entityId];
-          if (!entity) return;
+          // remove dots that are no longer in the world
+          keys(dots).forEach((id) => {
+            if (!w.entities[id]) {
+              container.removeChild(dots[id]);
+              delete dots[id];
+            }
+          });
 
-          const { position } = entity.components;
-          if (!position) return;
+          const playerCharacter = w.client?.playerCharacter();
+          if (!playerCharacter) return;
+          const playerPosition = playerCharacter.components.position;
 
-          // update positions
+          // update player dots
+          w.queryEntities(["player"]).forEach((entity) => {
+
+            if (!dots[entity.id]) {
+              const color = (entity.id === w.client?.playerId()) ? 0x00ff00 : 0xff0000;
+              dots[entity.id] = pixiGraphics().circle(0, 0, 3).fill({ color });
+              dots[entity.id].mask = mask;
+              container.addChild(dots[entity.id]);
+            }
+
+            const { controlling } = entity.components;
+            if (!controlling) return;
+
+            const character = w.entities[controlling.data.entityId];
+            if (!character) return;
+
+            const { position } = character.components;
+            if (!position) return;
+
+            if (fullscreen) dots[entity.id].position.set(position.data.x / 7.6 - 4, position.data.y / 3.8 + 2);
+
+            if (!fullscreen) {
+              if (entity.id === w.client?.playerId()) {
+                dots[entity.id].position.set(0, 0);
+              } else {
+                dots[entity.id].position.set((position.data.x - playerPosition.data.x) * scale / 5.6, (position.data.y - playerPosition.data.y) * scale / 2.8);
+              }
+            }
+          });
+
+          // update tile graphic position
           if (fullscreen) {
             tileGraphics.position.set(0, 0);
-            playerDot.position.set(position.data.x / 7.6 - 4, position.data.y / 3.8 + 2);
           } else {
-            playerDot.position.set(0, 0);
-            tileGraphics.position.set(-position.data.x / 5.6 * scale + 5, - position.data.y / 2.8 * scale + 2);
+            tileGraphics.position.set(-playerPosition.data.x / 5.6 * scale + 5, - playerPosition.data.y / 2.8 * scale + 2);
           }
         },
         setContainer: async () => {
@@ -94,7 +134,7 @@ export const Minimap = (dim: number, tileMap: number[]): Entity => {
             tileGraphics.rect(x * width, y * width, width, width).fill({ color });
           });
 
-          container.addChild(background, tileGraphics, outline, playerDot, mask);
+          container.addChild(background, tileGraphics, outline, mask);
           return container;
         }
       })
