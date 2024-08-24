1fb26db1842967700954ad143e36c0ce6c0a82f4
Alex <alex@piggo.gg>
2024-08-23 21:48:26 -0500

    v0.6.25 sound improvements (#169)

--- core/src/sound/Sounds.ts
+++ core/src/sound/Sounds.ts
@@ -3,8 +3,10 @@ import { Player } from "tone";
 
 export type Sound = Player;
 
+type WallPlaceSounds = "wallPlace1" | "wallPlace2";
 type ZombieDeathSounds = "zombieDeath1" | "zombieDeath2" | "zombieDeath3" | "zombieDeath4";
-export type ValidSounds = GunNames | ZombieDeathSounds;
+
+export type ValidSounds = GunNames | ZombieDeathSounds | WallPlaceSounds;
 export type Sounds = Record<ValidSounds, Sound>;
 
 const load = (url: string, volume: number): Sound => {
@@ -13,11 +15,13 @@ const load = (url: string, volume: number): Sound => {
 }
 
 export const Sounds = (): Sounds => ({
-  deagle: load("pistol.mp3", -40),
-  ak: load("ak.mp3", -30),
-  awp: load("awp.mp3", -40),
-  zombieDeath1: load("zombieDeath1.wav", -30),
-  zombieDeath2: load("zombieDeath2.wav", -30),
-  zombieDeath3: load("zombieDeath3.wav", -30),
-  zombieDeath4: load("zombieDeath4.wav", -30)
+  deagle: load("pistol.mp3", -30),
+  ak: load("ak.mp3", -25),
+  awp: load("awp.mp3", -30),
+  zombieDeath1: load("zombieDeath1.wav", -25),
+  zombieDeath2: load("zombieDeath2.wav", -25),
+  zombieDeath3: load("zombieDeath3.wav", -25),
+  zombieDeath4: load("zombieDeath4.wav", -25),
+  wallPlace1: load("wallPlace1.wav", -15),
+  wallPlace2: load("wallPlace2.wav", -15),
 })
