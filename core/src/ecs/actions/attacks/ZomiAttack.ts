ed4f5788215a84cb97f2e7b089aa3c2e7ae2f055
Alex <petrovich.alex96@gmail.com>
2024-09-01 20:04:41 -0500

    ship

--- core/src/ecs/actions/attacks/ZomiAttack.ts
+++ core/src/ecs/actions/attacks/ZomiAttack.ts
@@ -1,4 +1,4 @@
-import { Action, Entity, Sounds, ZomiAttackSounds, playSound, randomChoice } from "@piggo-gg/core";
+import { Action, Entity, ZomiAttackSounds, playSound, randomChoice } from "@piggo-gg/core";
 
 export const ZomiAttack = (damage: number, cooldown: number) => Action<{ target: Entity }>(({ entity, params, world }) => {
   const { target } = params;
@@ -6,7 +6,7 @@ export const ZomiAttack = (damage: number, cooldown: number) => Action<{ target:
 
   if (health) health.data.health -= damage;
 
-  entity?.components.position?.setHeading({ x: NaN, y: NaN });
+  entity?.components.position?.clearHeading();
 
   playSound(world.client?.sounds[randomChoice(["attack1", "attack2", "attack3", "attack4"]) as ZomiAttackSounds]);
 }, cooldown)
