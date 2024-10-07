2a2a4a138c7536894ae25861ad8f4023571290db
Alex <petrovich.alex96@gmail.com>
2024-10-07 15:33:45 +0800

    piggo eat

---
+++ core/src/ecs/actions/interactive/Eat.ts
@@ -0,0 +1,22 @@
+import { Action, Entity, Position } from "@piggo-gg/core";
+
+export const Eat = Action<{ target: Entity<Position> }>(({ entity, params, world }) => {
+  if (!entity) return;
+
+  const { target } = params;
+  if (!target || !world.entities[target.id]) return;
+
+  world.removeEntity(target.id);
+
+  if (entity.components.renderable?.scale) {
+    entity.components.renderable.scale += 1;
+  }
+
+
+  console.log("eating", entity.id)
+
+  const { position } = entity.components;
+  if (!position) return;
+
+  position.setHeading(target.components.position.data);
+});
