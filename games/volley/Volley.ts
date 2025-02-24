0a906a265c6b5f223221636e5821e05046136aeb
Alex <petrovich.alex96@gmail.com>
2025-02-24 16:43:57 -0600

    wip

--- games/volley/Volley.ts
+++ games/volley/Volley.ts
@@ -1,4 +1,8 @@
-import { Actions, Character, Collider, Entity, GameBuilder, Input, loadTexture, Move, Networked, Player, Point, Position, Renderable, SpawnSystem } from "@piggo-gg/core"
+import {
+  Action, Actions, Background, CameraSystem, Character, Collider, Debug, Entity, GameBuilder, Input,
+  LineWall, loadTexture, min, Move, Networked, Player, Point,
+  Position, Renderable, SpawnSystem, SystemBuilder, WASDInputMap
+} from "@piggo-gg/core"
 import { AnimatedSprite } from "pixi.js"
 
 // 2d volleyball game (2v2)
@@ -6,32 +10,43 @@ export const Volley: GameBuilder = {
   id: "volley",
   init: () => ({
     id: "volley",
-    systems: [SpawnSystem(Mouse)],
+    systems: [SpawnSystem(Mouse), GravitySystem, CameraSystem({ follow: () => ({ x: 225, y: 0 }) })],
     bgColor: 0x006633,
-    entities: []
+    entities: [Court(), Net(), Background({ img: "space.png" })]
   })
 }
 
 // the players are mice
-export const Mouse = (player: Player) => {
+const Mouse = (player: Player) => {
   const mouse: Character = Entity({
     id: `mouse-${player.id}`,
     components: {
-      position: Position({ x: 32, y: 100, velocityResets: 1, speed: 120 }),
+      debug: Debug(),
+      position: Position({ x: 0, y: 0, velocityResets: 1, speed: 120 }),
       networked: Networked(),
       collider: Collider({ shape: "ball", radius: 8, hittable: true }),
       team: player.components.team,
       input: Input({
         press: {
-          "a": () => ({ actionId: "move", params: { x: 120 } }),
-          "d": () => ({ actionId: "move", params: { x: -120 } }),
+          ...WASDInputMap.press,
+          " ": () => ({ actionId: "jump" })
         }
       }),
       actions: Actions<any>({
         move: Move,
-        point: Point
+        point: Point,
+        jump: Action("jump", ({ entity }) => {
+          console.log("jump")
+          if (!entity) return
+
+          const { renderable } = entity.components
+          if (!renderable) return
+
+          renderable.position.y -= 20
+        }, 10)
       }),
       renderable: Renderable({
+        anchor: { x: 0.5, y: 0.7 },
         scale: 2,
         zIndex: 3,
         interpolate: true,
@@ -40,8 +55,14 @@ export const Mouse = (player: Player) => {
           const t = await loadTexture("chars.json")
 
           r.animations = {
+            d: new AnimatedSprite([t["d1"], t["d2"], t["d3"]]),
+            u: new AnimatedSprite([t["u1"], t["u2"], t["u3"]]),
             l: new AnimatedSprite([t["l1"], t["l2"], t["l3"]]),
-            r: new AnimatedSprite([t["r1"], t["r2"], t["r3"]])
+            r: new AnimatedSprite([t["r1"], t["r2"], t["r3"]]),
+            dl: new AnimatedSprite([t["dl1"], t["dl2"], t["dl3"]]),
+            dr: new AnimatedSprite([t["dr1"], t["dr2"], t["dr3"]]),
+            ul: new AnimatedSprite([t["ul1"], t["ul2"], t["ul3"]]),
+            ur: new AnimatedSprite([t["ur1"], t["ur2"], t["ur3"]])
           }
         }
       })
@@ -49,3 +70,43 @@ export const Mouse = (player: Player) => {
   })
   return mouse
 }
+
+const Net = () => LineWall({
+  position: { x: 225, y: -75 },
+  points: [
+    0, 0,
+    0, 150
+  ],
+  visible: true
+})
+
+// court (trapezoid)
+const Court = () => LineWall({
+  position: { x: 0, y: -75 },
+  points: [
+    0, 0,
+    450, 0,
+    500, 150,
+    -50, 150,
+    0, 0
+  ],
+  visible: true,
+  fill: 0xE2CA76
+})
+
+const GravitySystem = SystemBuilder({
+  id: "GravitySystem",
+  init: (world) => ({
+    id: "GravitySystem",
+    query: ["position", "collider", "renderable"],
+    onTick: (entities: Entity<Position | Collider | Renderable>[]) => {
+      entities.forEach((entity) => {
+        const { position, collider, renderable } = entity.components
+
+        if (renderable.position.y < 0) {
+          renderable.position.y = min(0, renderable.position.y + 1)
+        }
+      })
+    }
+  })
+})
