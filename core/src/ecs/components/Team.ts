d78e147cfaffd670734c9bd99facc50463e43c35
Alex <petrovich.alex96@gmail.com>
2025-03-13 18:41:25 -0500

    wip

--- core/src/ecs/components/Team.ts
+++ core/src/ecs/components/Team.ts
@@ -30,7 +30,7 @@ export const TeamSystem: SystemBuilder<"TeamSystem"> = {
     return {
       id: "TeamSystem",
       query: ["team"],
-      priority: 5, // todo
+      priority: 5,
       onTick: (entities) => {
         entities.forEach((entity) => {
           const { team } = entity.components
