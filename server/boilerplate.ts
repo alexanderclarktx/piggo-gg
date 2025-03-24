5f56525e1b8fbdc5ed9d97de2ccfe38fd2a62501
Alex <petrovich.alex96@gmail.com>
2025-03-24 13:03:09 -0500

    wip

--- server/boilerplate.ts
+++ server/boilerplate.ts
@@ -1,5 +1,11 @@
-// to satisfy pixi-filters
+import { mocks } from 'mock-browser';
+
+// for pixi-filters
 global.window = {
   innerWidth: 1920,
   innerHeight: 1080
 }
+
+// for react-hot-toast
+const mock = new mocks.MockBrowser()
+global.document = mock.getDocument()
