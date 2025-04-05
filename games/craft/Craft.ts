7cb57cb41cc8dd35767e0bbb9fb18feb4b388a3f
Alex <petrovich.alex96@gmail.com>
2025-04-05 10:21:06 -0500

    wip

--- games/craft/Craft.ts
+++ games/craft/Craft.ts
@@ -1,6 +1,8 @@
 import {
   Piggo, SpawnSystem, Tree, isMobile, MobilePvEHUD, PvEHUD, Rock, Skelly,
-  GameBuilder, DefaultUI, CameraSystem, InventorySystem, ShadowSystem, Background
+  GameBuilder, DefaultUI, CameraSystem, InventorySystem, ShadowSystem, Background,
+  SystemBuilder,
+  Controlling
 } from "@piggo-gg/core"
 
 export const Craft: GameBuilder = {
@@ -9,7 +11,7 @@ export const Craft: GameBuilder = {
     id: "craft",
     netcode: "delay",
     state: {},
-    systems: [InventorySystem, ShadowSystem, CameraSystem(), SpawnSystem(Skelly)],
+    systems: [InventorySystem, ShadowSystem, CraftSystem, CameraSystem(), SpawnSystem(Skelly)],
     entities: [
       Background({ rays: true }),
       ...DefaultUI(world),
@@ -25,3 +27,25 @@ export const Craft: GameBuilder = {
     ]
   })
 }
+
+const CraftSystem = SystemBuilder({
+  id: "CraftSystem",
+  init: (world) => ({
+    id: "CraftSystem",
+    query: [],
+    priority: 3, // todo
+    onTick: () => {
+      const players = world.queryEntities<Controlling>(["pc", "controlling"])
+
+      for (const player of players) {
+        const character = player.components.controlling.getCharacter(world)
+        if (!character) continue
+        const { position } = character.components
+
+        console.log(position.data.z)
+      }
+      
+      // console.log("CraftSystem", entities)
+    }
+  }),
+})
