bdb1ca2808664048f299c3f79a9e7a26e5d16292
Alex <petrovich.alex96@gmail.com>
2025-07-22 21:44:37 -0500

    wip

--- games/dde/Bird.ts
+++ games/dde/Bird.ts
@@ -142,9 +142,9 @@ export const Bird = () => Character({
         if (position.data.flying) {
           factor = params.sprint ? 0.16 : 0.09
         } else if (position.data.standing) {
-          factor = params.sprint ? 0.65 : 0.45
+          factor = params.sprint ? 0.65 : 0.42
         } else {
-          factor = params.sprint ? 0.09 : 0.06
+          factor = params.sprint ? 0.09 : 0.056
         }
 
         position.impulse({ x: toward.x * factor, y: toward.z * factor })
