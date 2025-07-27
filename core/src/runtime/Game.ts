be0f0229faae1e9c499708d0dc554b47add6d839
Alex <petrovich.alex96@gmail.com>
2025-07-27 10:06:27 -0500

    wip

--- core/src/runtime/Game.ts
+++ core/src/runtime/Game.ts
@@ -1,7 +1,7 @@
 import { Command, Entity, SystemBuilder, World } from "@piggo-gg/core"
 
-export type Game<State extends {} = {}, T extends string = string> = {
-  id: T
+export type Game<State extends {} = {}, ID extends string = string> = {
+  id: ID
   bgColor?: number
   commands?: Command[]
   entities: Entity[]
@@ -15,3 +15,4 @@ export type GameBuilder<State extends {} = {}, T extends string = string> = {
   id: T
   init: (world: World) => Game<State, T>
 }
+  
