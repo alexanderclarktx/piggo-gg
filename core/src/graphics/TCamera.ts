5dfefa162a949707155b5d39e0c4e564c8a22e7c
Alex <petrovich.alex96@gmail.com>
2025-08-04 08:01:25 -0500

    wip

--- core/src/graphics/TCamera.ts
+++ core/src/graphics/TCamera.ts
@@ -7,39 +7,6 @@ export type TCamera = {
   setFov: (fov: number) => void
 }
 
-export const TCameraSystem = () => ClientSystemBuilder({
-  id: "TCameraSystem",
-  init: (world) => {
-    return {
-      id: "TCameraSystem",
-      query: [],
-      priority: 9,
-      onRender: (_, delta) => {
-        if (!world.three) return
-
-        const pc = world.client?.playerCharacter()
-        if (!pc) return
-
-        const { position } = pc.components
-
-        const interpolated = position.interpolate(world, delta)
-
-        const { x, y } = localAim
-
-        const rotatedOffset = new Vector3(-sin(x), y, -cos(x)).multiplyScalar(0.6)
-
-        world.three.camera.c.position.set(
-          interpolated.x - rotatedOffset.x,
-          interpolated.z + 0.4 - rotatedOffset.y,
-          interpolated.y - rotatedOffset.z
-        )
-
-        world.three.camera.c.lookAt(interpolated.x, interpolated.z + 0.2, interpolated.y)
-      }
-    }
-  }
-})
-
 export const TCamera = (): TCamera => {
 
   const camera = new PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -58,3 +25,34 @@ export const TCamera = (): TCamera => {
   }
   return tCamera
 }
+
+export const TCameraSystem = () => ClientSystemBuilder({
+  id: "TCameraSystem",
+  init: (world) => ({
+    id: "TCameraSystem",
+    query: [],
+    priority: 9,
+    onRender: (_, delta) => {
+      if (!world.three) return
+
+      const pc = world.client?.playerCharacter()
+      if (!pc) return
+
+      const { position } = pc.components
+
+      const interpolated = position.interpolate(world, delta)
+
+      const { x, y } = localAim
+
+      const rotatedOffset = new Vector3(-sin(x), y, -cos(x)).multiplyScalar(0.6)
+
+      world.three.camera.c.position.set(
+        interpolated.x - rotatedOffset.x,
+        interpolated.z + 0.4 - rotatedOffset.y,
+        interpolated.y - rotatedOffset.z
+      )
+
+      world.three.camera.c.lookAt(interpolated.x, interpolated.z + 0.2, interpolated.y)
+    }
+  })
+})
