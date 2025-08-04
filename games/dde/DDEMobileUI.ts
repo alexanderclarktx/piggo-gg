def7a28511423b4cbaa2bd26d622dd3d62516113
Alex <petrovich.alex96@gmail.com>
2025-08-04 08:55:47 -0500

    wip

--- games/dde/DDEMobileUI.ts
+++ games/dde/DDEMobileUI.ts
@@ -13,13 +13,13 @@ export const DDEMobileUI = (world: World) => {
 
   world.three?.append(HtmlText({
     text: "transform",
-    style: { left: "50%", bottom: "24px", fontSize: "16px" }
+    style: { left: "60%", bottom: "24px", fontSize: "14px" }
   }))
 
   const transformButton = HtmlButton({
     style: {
       bottom: "50px",
-      left: "50%",
+      left: "60%",
       transform: "translate(-50%)",
       backgroundColor: "rgba(255, 192, 203, 0.5)",
       width: "50px",
@@ -38,4 +38,32 @@ export const DDEMobileUI = (world: World) => {
   })
 
   world.three?.append(transformButton)
+
+  world.three?.append(HtmlText({
+    text: "jump",
+    style: { left: "40%", bottom: "24px", fontSize: "14px" }
+  }))
+
+  const jumpButton = HtmlButton({
+    style: {
+      bottom: "50px",
+      left: "40%",
+      transform: "translate(-50%)",
+      backgroundColor: "rgba(20, 255, 60, 0.5)",
+      width: "50px",
+      height: "50px",
+      borderRadius: "50%"
+    },
+    onClick: () => {
+      world.actions.push(
+        world.tick + 1, world.client?.playerCharacter()?.id ?? "", { actionId: "jump" }
+      )
+      jumpButton.style.backgroundColor = "rgba(20, 255, 60, 0.8)"
+    },
+    onRelease: () => {
+      jumpButton.style.backgroundColor = "rgba(20, 255, 60, 0.5)"
+    }
+  })
+
+  world.three?.append(jumpButton)
 }
