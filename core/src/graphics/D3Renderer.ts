896c580a9f3e12b976a2641da712288bd4ec66b4
Alex <petrovich.alex96@gmail.com>
2025-08-07 20:57:10 -0500

    wip

--- core/src/graphics/D3Renderer.ts
+++ core/src/graphics/D3Renderer.ts
@@ -199,22 +199,22 @@ export const D3Renderer = (c: HTMLCanvasElement): D3Renderer => {
       sunSphere.position.copy(sun.position)
       renderer.scene.add(sunSphere)
 
-      GL.load("Eagle-Golden.glb", (eagle) => {
+      GL.load("eagle.glb", (eagle) => {
         renderer.eagle = eagle.scene
 
         renderer.eagle.animations = eagle.animations
         eagle.scene.rotation.order = "YXZ"
 
-        // const colors: Record<string, number> = {
-        //   Cylinder: 0x5C2421,
-        //   Cylinder_1: 0xE7C41C,
-        //   Cylinder_2: 0xffffff,
-        //   Cylinder_3: 0x632724
-        // }
+        const colors: Record<string, number> = {
+          Cylinder: 0x5C2421,
+          Cylinder_1: 0xE7C41C,
+          Cylinder_2: 0xffffff,
+          Cylinder_3: 0x632724
+        }
 
         eagle.scene.traverse((child) => {
           if (child instanceof Mesh) {
-            // child.material = new MeshStandardMaterial({ color: colors[child.name] })
+            child.material = new MeshStandardMaterial({ color: colors[child.name] })
             child.castShadow = true
             child.receiveShadow = true
           }
