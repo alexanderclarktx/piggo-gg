b744dbea4bc6e37ebf33169cd95d238e75d65060
Alex <petrovich.alex96@gmail.com>
2025-08-13 19:50:50 -0500

    wip

--- core/src/html/HtmlChat.ts
+++ core/src/html/HtmlChat.ts
@@ -1,6 +1,34 @@
-import { Entity, NPC, Position } from "@piggo-gg/core";
+import { Entity, HtmlDiv, HtmlText, NPC, Position } from "@piggo-gg/core";
 
 export const HtmlChat = (): Entity => {
+
+  let init = false
+
+  const wrapper = HtmlDiv({
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    width: "300px",
+    height: "400px",
+    right: "10px",
+    bottom: "10px",
+    transform: "translate(0%)",
+    borderRadius: "8px",
+  })
+
+  const input = HtmlText({
+    text: "",
+    style: {
+      position: "absolute",
+      width: "280px",
+      height: "30px",
+      bottom: "10px",
+      left: "10px",
+      borderRadius: "8px",
+      
+      backgroundColor: "rgba(0, 0, 0, 0.5)"
+    }
+  })
+
+  wrapper.appendChild(input)
 
   const chat = Entity({
     id: "html-chat",
@@ -9,7 +37,10 @@ export const HtmlChat = (): Entity => {
       position: Position(),
       npc: NPC({
         behavior: (_, world) => {
-
+          if (!init) {
+            init = true
+            world.three?.append(wrapper)
+          }
         }
       })
     }
