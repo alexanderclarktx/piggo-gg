ee98dcb1b33ff5ed3321d47ba3853c82cd75d7d2
Alex <petrovich.alex96@gmail.com>
2025-08-16 21:11:33 -0500

    wip

--- core/src/ecs/entities/ui/Crosshair.ts
+++ core/src/ecs/entities/ui/Crosshair.ts
@@ -1,11 +1,33 @@
-import { Entity } from "@piggo-gg/core"
+import { Entity, HtmlDiv, NPC, Position } from "@piggo-gg/core"
 
 export const Crosshair = () => {
+
+  let init = false
 
   const crosshair = Entity({
     id: "crosshair",
     components: {
+      position: Position(),
+      npc: NPC({
+        behavior: (_, world) => {
+          if (!init) {
+            init = true
 
+            const div = HtmlDiv({
+              left: "50%",
+              top: "50%",
+              transform: "translate(-50%, -50%)",
+              width: "2px",
+              height: "2px",
+              backgroundColor: "#00ff00",
+              borderRadius: "50%",
+              pointerEvents: "none"
+            })
+
+            world.three?.append(div)
+          }
+        }
+      })
     }
   })
 }
