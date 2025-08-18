001443896f1a903284d3b8191b1e650af430f276
Alex <petrovich.alex96@gmail.com>
2025-08-18 16:00:19 -0500

    ship

--- core/src/ecs/actions/movement/Head.ts
+++
@@ -1,9 +0,0 @@
-import { Action, XY } from "@piggo-gg/core"
-
-export const Head = Action<XY>("head", ({ params, entity }) => {
-  if (!entity) return
-
-  const { position } = entity.components
-
-  position?.setHeading({ x: params.x, y: params.y })
-})
