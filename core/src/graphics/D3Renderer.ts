acc776097d009b608951ea3d816a1b6d05d4516b
Alex <petrovich.alex96@gmail.com>
2025-08-19 14:32:55 -0500

    wip

--- core/src/graphics/D3Renderer.ts
+++ core/src/graphics/D3Renderer.ts
@@ -143,7 +143,7 @@ export const D3Renderer = (c: HTMLCanvasElement): D3Renderer => {
       webgl.shadowMap.enabled = true
       webgl.shadowMap.type = 2
 
-      sun = new DirectionalLight(evening, 9)
+      sun = new DirectionalLight(evening, 10)
       renderer.scene.add(sun)
 
       sun.position.set(200, 100, 200)
