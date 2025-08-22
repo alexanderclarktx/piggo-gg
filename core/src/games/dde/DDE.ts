25bea02358c60d19f555d16a16842a9d895b0ffc
Alex <petrovich.alex96@gmail.com>
2025-08-22 15:53:01 -0500

    wip

--- core/src/games/dde/DDE.ts
+++ core/src/games/dde/DDE.ts
@@ -1,8 +1,7 @@
 import {
   BlockPhysicsSystem, D3Apple, D3CameraSystem, D3NametagSystem, GameBuilder, hypot,
   logPerf, min, PI, D3Profile, Random, randomInt, SpawnSystem, spawnTerrain, sqrt,
-  SystemBuilder, XYZdistance, HtmlChat, Crosshair, BlockTypeString, cloneThree,
-  EscapeMenu
+  SystemBuilder, XYZdistance, HtmlChat, Crosshair, BlockTypeString, cloneThree, EscapeMenu
 } from "@piggo-gg/core"
 import { AnimationMixer, Color, Group, Object3D, Object3DEventMap } from "three"
 import { clone } from "three/examples/jsm/utils/SkeletonUtils.js"
