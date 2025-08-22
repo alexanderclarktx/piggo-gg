20315715e8cf769a7b43d9593ba7cd2cf6fe9238
Alex <petrovich.alex96@gmail.com>
2025-08-22 17:42:17 -0500

    wip

--- core/src/html/HtmlInput.ts
+++ core/src/html/HtmlInput.ts
@@ -1,7 +1,18 @@
 import { HtmlText, CSS, HtmlDiv } from "@piggo-gg/core"
 
+const defaults: CSS = {
+  fontSize: "18px",
+  lineHeight: "40px"
+}
+
 export const HtmlInput = (style: CSS = {}): HtmlDiv => {
   const div = HtmlText()
+
+  div.contentEditable = "true"
+  div.inputMode = "text"
+
+  Object.assign(div.style, defaults)
+  Object.assign(div.style, style)
 
   return div
 }
