cf60d92d252169abb8594bd72ead9fad64d74cc0
Alex <petrovich.alex96@gmail.com>
2025-08-26 13:34:59 -0500

    wip

--- core/src/ecs/entities/menus/EscapeMenu.ts
+++ core/src/ecs/entities/menus/EscapeMenu.ts
@@ -21,7 +21,7 @@ export const EscapeMenu = (world: World): Entity => {
   const art = Art()
 
   const menuButtonStyle: CSS = {
-    width: "30%", position: "relative", top: "-10px", height: "40px", pointerEvents: "auto"
+    width: "33%", position: "relative", top: "-10px", height: "40px", pointerEvents: "auto"
   }
 
   const lobbiesButton = HtmlButton({
