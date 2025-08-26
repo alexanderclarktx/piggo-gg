1f27058f602f65cf2139c6134b33954549f0dfda
Alex <petrovich.alex96@gmail.com>
2025-08-26 14:01:39 -0500

    wip

--- core/src/games/dde/DDEMobileUI.ts
+++ core/src/games/dde/DDEMobileUI.ts
@@ -3,27 +3,8 @@ import { HtmlButton, HtmlJoystick, World } from "@piggo-gg/core"
 export const DDEMobileUI = (world: World) => {
   if (!world.client?.mobile || !world.three) return
 
-  const menuButton = HtmlButton({
-    text: "menu",
-    onClick: () => {
-      world.three!.mobileLock = !world.three!.mobileLock
-    },
-    style: {
-      top: "2%",
-      right: "2%",
-      width: "80px"
-    }
-  })
-
-  world.three.append(menuButton)
-
-  world.three?.append(
-    HtmlJoystick(world.client, "left")
-  )
-
-  world.three?.append(
-    HtmlJoystick(world.client, "right")
-  )
+  const leftJoystick = HtmlJoystick(world.client, "left")
+  const rightJoystick = HtmlJoystick(world.client, "right")
 
   const transformButton = HtmlButton({
     style: {
@@ -46,8 +27,6 @@ export const DDEMobileUI = (world: World) => {
     }
   })
 
-  world.three.append(transformButton)
-
   const jumpButton = HtmlButton({
     style: {
       bottom: "140px",
@@ -69,5 +48,25 @@ export const DDEMobileUI = (world: World) => {
     }
   })
 
-  world.three?.append(jumpButton)
+  world.three?.append(transformButton, jumpButton, leftJoystick, rightJoystick)
+
+  const menuButton = HtmlButton({
+    text: "menu",
+    onClick: () => {
+      world.three!.mobileLock = !world.three!.mobileLock
+
+      const visibility = world.three?.mobileLock ? "visible" : "hidden"
+      jumpButton.style.visibility = visibility
+      transformButton.style.visibility = visibility
+      leftJoystick.style.visibility = visibility
+      rightJoystick.style.visibility = visibility
+    },
+    style: {
+      top: "2%",
+      right: "2%",
+      width: "80px"
+    }
+  })
+
+  world.three.append(menuButton)
 }
