222d3b213bb435f92a4c72e0742b4d081584c3ec
Alex <petrovich.alex96@gmail.com>
2025-08-27 05:33:30 -0700

    ship

--- core/src/games/dde/DDEMobileUI.ts
+++ core/src/games/dde/DDEMobileUI.ts
@@ -60,6 +60,11 @@ export const DDEMobileUI = (world: World) => {
       transformButton.style.visibility = visibility
       leftJoystick.style.visibility = visibility
       rightJoystick.style.visibility = visibility
+
+      menuButton.style.backgroundColor = "rgba(0, 160, 255, 0.4)"
+    },
+    onRelease: () => {
+      menuButton.style.backgroundColor = "rgba(0, 0, 0 , 0.4)"
     },
     style: {
       marginTop: "env(safe-area-inset-top)",
