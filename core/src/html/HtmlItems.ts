9c9d2f8bb90e96e3cdab8712b91cb0cbd9942cd5
Alex <petrovich.alex96@gmail.com>
2025-08-30 10:39:13 -0700

    wip

--- core/src/html/HtmlItems.ts
+++ core/src/html/HtmlItems.ts
@@ -1,5 +1,14 @@
-import { Client } from "@piggo-gg/core"
+import { Client, HtmlDiv } from "@piggo-gg/core"
 
 export const HtmlItems = (client: Client) => {
 
+  const div = HtmlDiv({
+    width: "400px",
+    height: "50px",
+    bottom: "50px",
+    left: "50%",
+    transform: "translate(-50%)"
+  })
+
+  return div
 }
