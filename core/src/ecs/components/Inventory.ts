3019ecbddb9154661bc0f8fad6af9c5ace11cf08
Alex <petrovich.alex96@gmail.com>
2025-08-31 14:30:12 -0700

    wip

--- core/src/ecs/components/Inventory.ts
+++ core/src/ecs/components/Inventory.ts
@@ -20,7 +20,7 @@ export type Inventory = Component<"inventory", {
 export const Inventory = (itemBuilders: ItemBuilder[] = []): Inventory => {
   const inventory: Inventory = {
     data: {
-      activeItemIndex: 1,
+      activeItemIndex: 0,
       items: [undefined, undefined, undefined, undefined, undefined]
     },
     type: "inventory",
