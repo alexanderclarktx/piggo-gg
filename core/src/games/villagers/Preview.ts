0c210784d2d777583aea48d053386cc44272d8f4
Alex <petrovich.alex96@gmail.com>
2025-09-04 19:02:27 -0700

    wip

--- core/src/games/villagers/Preview.ts
+++ core/src/games/villagers/Preview.ts
@@ -1,9 +1,11 @@
+import { blockFromXYZ, floor, hypot, min, World, XYZ } from "@piggo-gg/core"
 import { Mesh, MeshBasicMaterial, Object3DEventMap, PlaneGeometry } from "three"
 
 
 // Mesh<CylinderGeometry, MeshBasicMaterial, Object3DEventMap>
 export type Preview = {
   mesh: Mesh<PlaneGeometry, MeshBasicMaterial, Object3DEventMap>
+  update: (world: World, pos: XYZ, dir: XYZ) => void
 }
 
 export const Preview = (): Preview => {
@@ -11,5 +13,52 @@ export const Preview = (): Preview => {
   const material = new MeshBasicMaterial({ color: 0xffffff, side: 2 })
 
   const mesh = new Mesh(geometry, material)
-  return { mesh }
+  return {
+    mesh,
+    update: (world: World, pos: XYZ, dir: XYZ) => {
+      // mesh.position.copy(pos)
+      // mesh.lookAt(aim)
+
+      const current = { ...pos }
+
+      const lastBlock = blockFromXYZ(current)
+
+      let travelled = 0
+      let cap = 10
+
+      while (travelled < 10 && cap > 0) {
+
+        const xGap = (current.x + 0.15) % 0.3
+        const yGap = (current.y + 0.15) % 0.3
+        const zGap = (current.z + 0.15) % 0.3
+
+        const xStep = dir.x > 0 ? (0.3 - xGap) / dir.x : (xGap / -dir.x)
+        const yStep = dir.z > 0 ? (0.3 - yGap) / dir.z : (yGap / -dir.z)
+        const zStep = dir.y > 0 ? (0.3 - zGap) / dir.y : (zGap / -dir.y)
+
+        const minStep = min(xStep, yStep, zStep)
+
+        const xDist = dir.x * minStep * 1.01
+        const yDist = dir.z * minStep * 1.01
+        const zDist = dir.y * minStep * 1.01
+
+        current.x += xDist
+        current.y += yDist
+        current.z += zDist
+
+        travelled += hypot(xDist, yDist, zDist)
+        cap -= 1
+
+        const insideBlock = {
+          x: floor((0.15 + current.x) / 0.3),
+          y: floor((0.15 + current.y) / 0.3),
+          z: floor(current.z / 0.3)
+        }
+
+        const type = world.blocks.atIJK(insideBlock)
+        if (type) {
+        }
+      }
+    }
+  }
 }
