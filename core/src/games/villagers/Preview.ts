b184907b5bb7105f1b0e585eb6f632d4f02f2435
Alex <petrovich.alex96@gmail.com>
2025-09-04 18:59:01 -0700

    wip

--- core/src/games/villagers/Preview.ts
+++ core/src/games/villagers/Preview.ts
@@ -1,13 +1,15 @@
-import { Mesh } from "three"
+import { Mesh, MeshBasicMaterial, Object3DEventMap, PlaneGeometry } from "three"
 
 
 // Mesh<CylinderGeometry, MeshBasicMaterial, Object3DEventMap>
 export type Preview = {
-  mesh: Mesh
+  mesh: Mesh<PlaneGeometry, MeshBasicMaterial, Object3DEventMap>
 }
 
 export const Preview = (): Preview => {
-  const mesh = new Mesh()
+  const geometry = new PlaneGeometry(0.3, 0.3)
+  const material = new MeshBasicMaterial({ color: 0xffffff, side: 2 })
+
+  const mesh = new Mesh(geometry, material)
   return { mesh }
 }
-
