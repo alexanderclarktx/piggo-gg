873673697b7fd7393c1fb29df5be4a5ae4c8f83d
Alex <petrovich.alex96@gmail.com>
2025-09-22 21:40:40 -0400

    wip

--- core/src/ecs/entities/blocks/BlockMesh.ts
+++ core/src/ecs/entities/blocks/BlockMesh.ts
@@ -5,13 +5,15 @@ import {
 export type BlockMesh = InstancedMesh<BoxGeometry, MeshPhysicalMaterial[], InstancedMeshEventMap>
 
 export const BlockMesh = (maxCount: number): BlockMesh => {
-  const geometry = new BoxGeometry(0.3, 0.3, 0.3)
-
   const mat = () => new MeshPhysicalMaterial({
     vertexColors: false, visible: false, specularIntensity: 0.05, wireframe: false
   })
 
-  const mesh = new InstancedMesh(geometry, [mat(), mat(), mat(), mat(), mat(), mat()], maxCount)
+  const mesh = new InstancedMesh(
+    new BoxGeometry(0.3, 0.3, 0.3),
+    [mat(), mat(), mat(), mat(), mat(), mat()],
+    maxCount
+  )
 
   mesh.castShadow = true
   mesh.receiveShadow = true
