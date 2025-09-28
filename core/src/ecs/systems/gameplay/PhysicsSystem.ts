6c0f51438e27914774b1a4707e4a0f2e3662da95
Alex <petrovich.alex96@gmail.com>
2025-09-28 14:03:49 -0500

    wip

--- core/src/ecs/systems/gameplay/PhysicsSystem.ts
+++ core/src/ecs/systems/gameplay/PhysicsSystem.ts
@@ -142,7 +142,7 @@ export const PhysicsSystem = (mode: "global" | "local") => SystemBuilder({
           if (position.data.velocityResets && (abs(diffX) > 1 || abs(diffY) > 1)) {
             if (sign(linvel.y) !== sign(position.data.velocity.y) && sign(linvel.x) !== sign(position.data.velocity.x)) {
               position.lastCollided = world.tick
-              continue
+              // continue
             }
           }
 
