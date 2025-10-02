b8087043689d559f0c459be25bd8ba8b483cc0ae
Alex <petrovich.alex96@gmail.com>
2025-10-02 14:16:07 -0500

    ship

--- core/src/games/lobby/Lobby.ts
+++ core/src/games/lobby/Lobby.ts
@@ -17,7 +17,7 @@ export const Lobby: GameBuilder = {
     renderer: "pixi",
     settings: {},
     state: {
-      gameId: "strike"
+      gameId: "lobby"
     },
     systems: [PixiRenderSystem],
     entities: [
