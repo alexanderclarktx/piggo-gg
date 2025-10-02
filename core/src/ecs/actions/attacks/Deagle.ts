6d3fb6e0657f1b6b7a59047fb75cc6b0ddc0083c
Alex <petrovich.alex96@gmail.com>
2025-10-02 10:30:54 -0500

    wip

--- core/src/ecs/actions/attacks/Deagle.ts
+++ core/src/ecs/actions/attacks/Deagle.ts
@@ -1,5 +1,5 @@
 import { Action, Actions, blockInLine, Character, cos, Effects, Input, Item, ItemEntity, max, min, Networked, playerForCharacter, Position, sin, sqrt, StrikeState, Target, Three, XY, XYZ, XYZdistance, XYZdot, XYZsub } from "@piggo-gg/core"
-import { Object3D, Vector3 } from "three"
+import { Mesh, MeshBasicMaterial, Object3D, Scene, SphereGeometry, Vector3 } from "three"
 
 const modelOffset = (localAim: XY, tip: boolean = false): XYZ => {
   const dir = { x: sin(localAim.x), y: cos(localAim.x), z: sin(localAim.y) }
@@ -24,9 +24,49 @@ export const DeagleItem = ({ character }: { character: Character }) => {
 
   let gun: undefined | Object3D = undefined
 
+  const particles: { mesh: Mesh, velocity: XYZ }[] = []
+
   const { inventory } = character.components
 
   let recoil = 0
+
+  const spawnParticles = (pos: XYZ, normal: Vector3) => {
+    const proto = particles[0]
+    if (!proto) return
+
+    for (let i = 0; i < 10; i++) {
+      const clone = proto.mesh.clone()
+      // const material = proto.material
+      // material.transparent = true;
+
+      // const mesh = new Mesh(particleGeometry, material) as Particle;
+      // mesh.position.copy(position);
+
+      // random direction around the normal
+      const dir = normal
+        .clone()
+        .add(
+          new Vector3(
+            (Math.random() - 0.5) * 0.5,
+            (Math.random() - 0.5) * 0.5,
+            (Math.random() - 0.5) * 0.5
+          )
+        )
+        .normalize();
+      
+      clone.position.set(pos.x, pos.y, pos.z)
+
+      particles.push({ mesh: clone, velocity: dir.multiplyScalar(0.1 + Math.random() * 0.3) })
+
+      // mesh.userData = {
+      //   velocity: dir.multiplyScalar(0.1 + Math.random() * 0.3),
+      //   life: 0.5 + Math.random() * 0.5, // seconds
+      // };
+
+      // scene.add(mesh);
+      // particles.push(mesh);
+    }
+  }
 
   const item = ItemEntity({
     id: `deagle-${character.id}`,
@@ -78,50 +118,55 @@ export const DeagleItem = ({ character }: { character: Character }) => {
           const { pos, aim, targets } = params
 
           const eyePos = { x: pos.x, y: pos.y, z: pos.z + 0.5 }
-            const eyes = new Vector3(eyePos.x, eyePos.z, eyePos.y)
-          
-            const target = new Vector3(
-              -sin(aim.x) * cos(aim.y), sin(aim.y), -cos(aim.x) * cos(aim.y)
-            ).normalize().multiplyScalar(10).add(eyes)
-          
-            const dir = target.clone().sub(eyes).normalize()
-          
-            const beamResult = blockInLine({ from: eyePos, dir, world, cap: 40 })
-            if (beamResult) {
-              world.blocks.remove(beamResult.inside)
-            }
-          
-            // if (world.client && entity.id !== world.client.character()?.id) return
-            // if (world.client) return
-          
-            for (const target of targets) {
-              const targetEntity = world.entity<Position>(target.id)
-              if (!targetEntity) continue
-          
-              const targetXYZ = { x: target.x, y: target.y, z: target.z + 0.05 }
-          
-              const L = XYZsub(targetXYZ, eyePos)
-              const tc = XYZdot(L, { x: dir.x, y: dir.z, z: dir.y })
-          
-              if (tc < 0) continue
-          
-              const Ldist = XYZdistance(targetXYZ, eyePos)
-              const D = sqrt((Ldist * Ldist) - (tc * tc))
-          
-              if (D > 0 && D < 0.08) {
-                // state.hit[target.id] = { tick: world.tick, by: entity.id }
-                const targetPlayer = playerForCharacter(world, target.id)
+          const eyes = new Vector3(eyePos.x, eyePos.z, eyePos.y)
 
-                console.log("hit", targetPlayer?.id)
-                // world.announce(`${player?.components.pc.data.name} hit ${targetPlayer?.components.pc.data.name}`)
-          
-                // targetEntity.components.position.data.flying = false
-              }
+          const target = new Vector3(
+            -sin(aim.x) * cos(aim.y), sin(aim.y), -cos(aim.x) * cos(aim.y)
+          ).normalize().multiplyScalar(10).add(eyes)
+
+          const dir = target.clone().sub(eyes).normalize()
+
+          const beamResult = blockInLine({ from: eyePos, dir, world, cap: 40 })
+          if (beamResult) {
+            world.blocks.remove(beamResult.inside)
+          }
+
+          // if (world.client && entity.id !== world.client.character()?.id) return
+          // if (world.client) return
+
+          for (const target of targets) {
+            const targetEntity = world.entity<Position>(target.id)
+            if (!targetEntity) continue
+
+            const targetXYZ = { x: target.x, y: target.y, z: target.z + 0.05 }
+
+            const L = XYZsub(targetXYZ, eyePos)
+            const tc = XYZdot(L, { x: dir.x, y: dir.z, z: dir.y })
+
+            if (tc < 0) continue
+
+            const Ldist = XYZdistance(targetXYZ, eyePos)
+            const D = sqrt((Ldist * Ldist) - (tc * tc))
+
+            if (D > 0 && D < 0.08) {
+              // state.hit[target.id] = { tick: world.tick, by: entity.id }
+              const targetPlayer = playerForCharacter(world, target.id)
+
+              console.log("hit", targetPlayer?.id)
+              // world.announce(`${player?.components.pc.data.name} hit ${targetPlayer?.components.pc.data.name}`)
+
+              // targetEntity.components.position.data.flying = false
             }
+          }
         }),
       }),
       three: Three({
         init: async (_, __, three) => {
+          // particles
+          const particleGeometry = new SphereGeometry(0.02, 6, 6);
+          const particleMaterial = new MeshBasicMaterial({ color: 0xffaa00 });
+
+          // gun
           three.gLoader.load("laser-gun.glb", (gltf) => {
             gun = gltf.scene
             gun.scale.set(0.03, 0.03, 0.03)
