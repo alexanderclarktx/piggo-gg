eaaa4c994f89405d007e318b6ff898ffcd20846f
Alex <petrovich.alex96@gmail.com>
2025-10-09 15:00:27 -0500

    wip

--- core/src/games/strike/Sarge.ts
+++ core/src/games/strike/Sarge.ts
@@ -13,13 +13,7 @@ const leap = 0.3
 
 export const Sarge = (player: Player): Character => {
 
-  let hitboxes: {
-    body: undefined | Mesh
-    head: undefined | Mesh
-  } = { body: undefined, head: undefined }
-
-  // let bodyHitbox: Mesh | undefined
-  // let headHitbox: Mesh | undefined
+  let hitboxes: { body: undefined | Mesh, head: undefined | Mesh } = { body: undefined, head: undefined }
 
   let pig: Object3D = new Object3D()
 
@@ -230,8 +224,10 @@ export const Sarge = (player: Player): Character => {
 
           // position
           pig.position.set(interpolated.x, interpolated.z + 0, interpolated.y)
-          hitboxes.body?.position.set(interpolated.x, interpolated.z + 0.2, interpolated.y)
-          hitboxes.head?.position.set(interpolated.x, interpolated.z + 0.45, interpolated.y)
+          if (world.debug) {
+            hitboxes.body?.position.set(interpolated.x, interpolated.z + 0.2, interpolated.y)
+            hitboxes.head?.position.set(interpolated.x, interpolated.z + 0.46, interpolated.y)
+          }
 
           // rotation
           pig.rotation.y = orientation.x + PI
@@ -265,12 +261,12 @@ export const Sarge = (player: Player): Character => {
         init: async (entity, world, three) => {
 
           // body
-          const bodyGeo = new CapsuleGeometry(0.09, 0.18)
+          const bodyGeo = new CapsuleGeometry(0.08, 0.18)
           const bodyMat = new MeshPhongMaterial({ color: 0x0000ff, transparent: true, opacity: 0.5 })
           hitboxes.body = new Mesh(bodyGeo, bodyMat)
 
           // head
-          const headGeo = new CapsuleGeometry(0.08, 0.06)
+          const headGeo = new CapsuleGeometry(0.08, 0.05)
           const headMat = new MeshPhongMaterial({ color: 0xff0000, transparent: true, opacity: 0.5 })
           hitboxes.head = new Mesh(headGeo, headMat)
 
