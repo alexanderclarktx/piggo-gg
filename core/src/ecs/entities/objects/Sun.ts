507e1043ab49eec883cfdbb3634df4a6699fd591
Alex <petrovich.alex96@gmail.com>
2025-10-09 14:18:40 -0500

    wip

--- core/src/ecs/entities/objects/Sun.ts
+++ core/src/ecs/entities/objects/Sun.ts
@@ -1,7 +1,11 @@
 import { colors, Entity, Position, Three } from "@piggo-gg/core"
-import { DirectionalLight, HemisphereLight, Mesh, MeshPhysicalMaterial, SphereGeometry } from "three"
+import { CameraHelper, DirectionalLight, HemisphereLight, Mesh, MeshPhysicalMaterial, SphereGeometry } from "three"
 
-export const Sun = () => {
+export type SunProps = {
+  bounds?: { left: number, right: number, top: number, bottom: number }
+}
+
+export const Sun = (props: SunProps = {}) => {
   const sun = Entity<Three>({
     id: "sun",
     components: {
@@ -15,10 +19,10 @@ export const Sun = () => {
           light.castShadow = true
 
           // widen the shadow
-          light.shadow.camera.left = -20
-          light.shadow.camera.right = 20
-          light.shadow.camera.top = 30
-          light.shadow.camera.bottom = -30
+          light.shadow.camera.left = props.bounds?.left ?? -20
+          light.shadow.camera.right = props.bounds?.right ?? 20
+          light.shadow.camera.top = props.bounds?.top ?? 30
+          light.shadow.camera.bottom = props.bounds?.bottom ?? -30
 
           const sphere = new Mesh(
             new SphereGeometry(8, 32, 32),
@@ -33,7 +37,9 @@ export const Sun = () => {
 
           const hemi = new HemisphereLight(0xaaaabb, colors.evening, 3)
 
-          sun.components.three.o.push(light, sphere, hemi)
+          const helper = new CameraHelper(light.shadow.camera)
+
+          sun.components.three.o.push(light, sphere, hemi, helper)
         }
       })
     }
