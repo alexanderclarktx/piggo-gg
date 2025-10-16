d5c7e7e115cbd160c35066e408ee05f9341d92f0
Alex <petrovich.alex96@gmail.com>
2025-10-16 11:03:11 -0500

    wip

--- core/src/html/HtmlAmmo.ts
+++ core/src/html/HtmlAmmo.ts
@@ -3,17 +3,15 @@ import { Entity, HDiv, HText, NPC, Position } from "@piggo-gg/core"
 export const HtmlAmmo = (): Entity => {
   let init = false
 
-  const wrapper = HDiv({
-    style: {
-      width: "fit-content",
-      height: "36px",
-      right: "15px",
-      bottom: "15px",
-      flexDirection: "row",
-      display: "flex",
-      alignItems: "center"
-    }
+  const clipText = HText({
+    style: { width: "400px" }
   })
+
+  const wrapper = HDiv({
+    style: { left: "50%", bottom: "40px" }
+  },
+    clipText
+  )
 
   return Entity({
     id: "htmlAmmo",
@@ -29,62 +27,13 @@ export const HtmlAmmo = (): Entity => {
           const pc = world.client?.character()
           const activeItem = pc?.components.inventory?.activeItem(world)
           const ammo = activeItem?.components.gun?.data.ammo
-          if (!ammo) {
+
+          if (ammo === undefined) {
             wrapper.style.display = "none"
             return
           }
 
-          // const total = ammo.reserve + (ammo.clip || 0)
-          // const clip = ammo.clip || 0
-
-          const clipText = HText({
-            text: `${ammo} / 0`,
-            style: {
-              fontSize: "32px",
-              color: "white",
-              fontWeight: "bold",
-              lineHeight: "36px",
-              textAlign: "right",
-              width: "fit-content",
-              height: "36px",
-              marginRight: "4px"
-            }
-          })
-
-          // const slashText = HText({
-          //   innerText: "/",
-          //   style: {
-          //     fontSize: "32px",
-          //     color: "white",
-          //     textShadow: "0 0 6px black",
-          //     fontFamily: "Arial, sans-serif",
-          //     fontWeight: "bold",
-          //     lineHeight: "36px",
-          //     textAlign: "right",
-          //     width: "fit-content",
-          //     height: "36px",
-          //     marginRight: "4px"
-          //   }
-          // })
-
-          // const totalText = HText({
-          //   innerText: `${total}`,
-          //   style: {
-          //     fontSize: "32px",
-          //     color: total <= 0 ? "#ff4444" : "white",
-          //     textShadow: "0 0 6px black",
-          //     fontFamily: "Arial, sans-serif",
-          //     fontWeight: "bold",
-          //     lineHeight: "36px",
-          //     textAlign: "right",
-          //     width: "fit-content",
-          //     height: "36px"
-          //   }
-          // })
-
-          wrapper.appendChild(clipText)
-          // wrapper.appendChild(slashText)
-          // wrapper.appendChild(totalText)
+          clipText.textContent = `${ammo} / 0`
         }
       }),
     }
